6ad72578a6ce0f22609b39b4d3888fb361a97a0c
aquabluefish <aquabluefish@gmail.com>
2014-09-29 19:47:10 +0900

    maxOverScrollYを、XMLで設定可能に

--- src/hm/orz/bluefish/overscrolllistview/CustomListView.java
+++ src/hm/orz/bluefish/overscrolllistview/CustomListView.java
@@ -6,23 +6,25 @@ import android.widget.ListView;
 
 public class CustomListView extends ListView {
 
- private int mOverscrollDistance = 200;
+	private int maxOverScrollY = 0;
 
-    public CustomListView(Context context) {
-        super(context);
-    }
+	public CustomListView(Context context) {
+		super(context);
+	}
 
-    public CustomListView(Context context, AttributeSet attrs) {
-     super(context, attrs);
-    }
+	public CustomListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		this.maxOverScrollY = attrs.getAttributeIntValue(null, "maxOverScrollY", 0);    }
 
-    public CustomListView(Context context, AttributeSet attrs, int defStyle) {
-        super(context, attrs, defStyle);
-    }
+	public CustomListView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		this.maxOverScrollY = attrs.getAttributeIntValue(null, "maxOverScrollY", 0);    }
 
 
- @Override
- protected boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY, int scrollRangeX, int scrollRangeY, int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
-  return super.overScrollBy(deltaX, deltaY, scrollX, scrollY, scrollRangeX, scrollRangeY, maxOverScrollX, mOverscrollDistance, isTouchEvent);
- }
+	@Override
+	protected boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY,
+			int scrollRangeX, int scrollRangeY, int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
+		return super.overScrollBy(deltaX, deltaY, scrollX, scrollY,
+				scrollRangeX, scrollRangeY, maxOverScrollX, this.maxOverScrollY, isTouchEvent);
+	}
 }
